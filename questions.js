3317e5e356f12eb6385d4e5bf3e104dec0ed3926
Shannon Ervin <sjervin07@gmail.com>
2019-11-15 00:00:00 -0600

    Questions added

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {    
+    title: "What Great House are Robb, Sansa, Arya, Bran and Rickon from?",
+    choices: ["Greyjoy", "Stark", "Baratheon", "Lannister"],
+    answer: "Stark"
+    },
+    {
+    title: "What is the capital of the Seven Kingdoms?",
+    choices: ["Dragonstone", "Winterfell", "Dorne", "King's Landing"],
+    answer: "King's Landing"
+    },
+    {
+    title: "Which of the following options is NOT one of Daenerys Targaryen's dragons?",
+    choices: ["Viserion", "Rhaegal", "Puff", "Drogon"],
+    answer: "Puff"
+    },
+    {
+    title: "The Dothraki are skilled warriors famous for using what animal in battle?",
+    choices: ["Elephants", "Ostriches", "Horses", "Camels"],
+    answer: "Horses"
+    },
+    {
+    title: "Who winds up on the throne at the end of the series?",
+    choices: ["Bran Stark", "Daenerys Targaryen", "Jon Snow", "Jamie Lannister"],
+    answer: "Bran Stark"
+    }
+    ];
